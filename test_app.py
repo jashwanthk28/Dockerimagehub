8ad80a810b5bdbbcc3a288886c01993417578364
nikhilshastry2003 <nikhilshastry2003@gmail.com>
2025-02-03 00:07:00 +0530

    revised testapp file

--- test_app.py
+++ test_app.py
@@ -4,5 +4,7 @@ from app import app
 
 def test_come():
     response = app.test_client().get("/")
+    
+
     assert response.status_code == 200
-    assert response.data == "Hello, World!"
+    assert response.data == b"Hello, World!"
