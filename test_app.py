e9d2114e94407ddb382e9ac5f2d68aea95cff636
nikhilshastry2003 <nikhilshastry2003@gmail.com>
2025-02-03 00:02:19 +0530

    fixed error in test-app file

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@
 from app import app
 
 def test_come():
-    responce = app.test_client.get("/")
-    assert responce.status_code == 200
-    assert responce.data == "Hello, World!"
+    response = app.test_client.get("/")
+    assert response.status_code == 200
+    assert response.data == "Hello, World!"
